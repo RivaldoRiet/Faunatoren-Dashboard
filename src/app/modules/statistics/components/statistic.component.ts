02e01ed3dc8fc457e2f13c7652bc337988f6cbb6
Rivaldo <rivaldo.sichtman@indicia.nl>
2021-03-03 19:21:11 +0100

    grafieken enzo

--- src/app/modules/statistics/components/statistic.component.ts
+++ src/app/modules/statistics/components/statistic.component.ts
@@ -9,13 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class StatisticComponent implements OnInit, OnDestroy {
   public currentId: string;
 
-  surveyData = [
-    { name: 'Huismussen', value: 105 },
-    { name: 'Zwaluwen', value: 350 },
-    { name: 'Spreeuwen', value: 170 },
-    { name: 'Vinken', value: 140 },
-    { name: 'Koolmeesjes', value: 200 }
-  ];
+
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
@@ -31,4 +25,106 @@ export class StatisticComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
 
   }
+
+
+
+
+  view: any[] = [700, 400];
+
+  // options for the chart
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Number';
+  showYAxisLabel = true;
+  yAxisLabel = 'Value';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // line, area
+  autoScale = true;
+
+  //pie
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+
+
+
+
+
+  surveyData = [
+    { name: 'Huismussen', value: 105 },
+    { name: 'Zwaluwen', value: 350 },
+    { name: 'Spreeuwen', value: 170 },
+    { name: 'Vinken', value: 140 },
+    { name: 'Koolmeesjes', value: 200 }
+  ];
+
+  public single = [
+    {
+      "name": "Huismussen",
+      "value": 89
+    },
+    {
+      "name": "Zwaluwen",
+      "value": 81
+    },
+    {
+      "name": "Spreeuwen",
+      "value": 87
+    }
+  ];
+  
+  public multi = [
+    {
+      "name": "Huismussen",
+      "series": [
+        {
+          "name": "2020",
+          "value": 73
+        },
+        {
+          "name": "2021",
+          "value": 89
+        }
+      ]
+    },
+  
+    {
+      "name": "Zwaluwen",
+      "series": [
+        {
+          "name": "2020",
+          "value": 78
+        },
+        {
+          "name": "2021",
+          "value": 82
+        }
+      ]
+    },
+  
+    {
+      "name": "Spreeuwen",
+      "series": [
+        {
+          "name": "2020",
+          "value": 52
+        },
+        {
+          "name": "2021",
+          "value": 94
+        }
+      ]
+    }
+  ];
+
+  
+
 }
