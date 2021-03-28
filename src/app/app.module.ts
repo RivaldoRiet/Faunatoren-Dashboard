4ed05841cc8be8d0dff1873dcde45849d2d82d3d
Rivaldo <rietcoins@inbox.lt>
2021-03-28 11:37:52 +0200

    birdnet first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BirdnetComponent } from './modules/birdnet/components/birdnet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirdnetComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/birdnet/components/birdnet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-birdnet',
+  templateUrl: './birdnet.component.html',
+  styleUrls: ['./birdnet.component.scss']
+})
+export class BirdnetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
