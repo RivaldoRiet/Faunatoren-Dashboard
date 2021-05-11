a2ea4d298c464c7d53aa46c443c35c9569c0a53e
Luis <louis.smid@hotmail.com>
2021-05-12 01:43:53 +0200

    Stylin + fake data
    fake ass data

--- src/app/modules/vogelhok2/components/vogelhok2.component.ts
+++ src/app/modules/vogelhok2/components/vogelhok2.component.ts
@@ -13,63 +13,159 @@ export class Vogelhok2Component implements OnInit {
   public currentId: string;
 
   surveyData = [
-    { name: 'Ma', value: 83 },
-    { name: 'Di', value: 150 },
-    { name: 'Wo', value: 64 },
-    { name: 'Do', value: 48 },
-    { name: 'Vr', value: 74 },
-    { name: 'Za', value: 106 },
-    { name: 'Zo', value: 89 }
+    { name: 'Ma', value: 43 },
+    { name: 'Di', value: 80 },
+    { name: 'Wo', value: 12 },
+    { name: 'Do', value: 0 },
+    { name: 'Vr', value: 0 },
+    { name: 'Za', value: 0 },
+    { name: 'Zo', value: 0 }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
     this.data = [
+      // // template
+      // {
+      //   datetime: new Date('2020-05-11 06:29:36'),
+      //   header: 'UIT',
+      //   body: [
+      //     // {
+      //     //   tag: 'h1',
+      //     //   content: "Lorem ipsum"
+      //     // },
+      //     // {
+      //     //   tag: 'p',
+      //     //   content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa.'
+      //     // }
+      //   ]
+      //     ,
+      //   // footer: 'Sample of footer. See <a href=\"https://github.com/Albejr/ngx-timeline\" target=\"_blank\">more details</a>'
+      // }
       {
-        datetime: new Date('2020-03-29 23:59:59'),
-        header: 'Sample of header',
-        body: [
-          // {
-          //   tag: 'h1',
-          //   content: "Lorem ipsum"
-          // },
-          {
-            tag: 'p',
-            content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa.'
-          }],
-        footer: 'Sample of footer. See <a href=\"https://github.com/Albejr/ngx-timeline\" target=\"_blank\">more details</a>'
-      },
-      {
-        datetime: new Date('2020-03-28 23:49:59'),
+        datetime: new Date('2020-05-12 12:15:12'),
         header: 'IN',
         body: [
-          // {
-          //   tag: 'h1',
-          //   content: "Lorem ipsum"
-          // },
+          {
+            tag: 'h1',
+            content: "IN"
+          },
           {
             tag: 'p',
-            content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa.'
-          }],
-        footer: 'Sample of footer. See <a href=\"https://github.com/Albejr/ngx-timeline\" target=\"_blank\">more details</a>'
+            content: 'De zwaluw heeft het hok verlaten.'
+          }
+        ]
+          ,
       },
       {
-        datetime: new Date('2020-03-27 23:39:59'),
-        header: 'Sample of header',
+        datetime: new Date('2020-05-12 10:30:20'),
+        header: 'UIT',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-12 9:12:50'),
+        header: 'IN',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-12 07:12:50'),
+        header: 'UIT',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-11 21:34:50'),
+        header: 'IN',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-11 19:14:52'),
+        header: 'UIT',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-11 19:04:10'),
+        header: 'IN',
+        body: [],
+      },
+      {
+        datetime: new Date('2020-05-11 18:23:47'),
+        header: 'UIT',
         body: [
-          // {
-          //   tag: 'h1',
-          //   content: "Lorem ipsum"
-          // },
-          {
-            tag: 'p',
-            content: 'Lorem ipsum dolor sit amet, nisl lorem, wisi egestas orci tempus class massa.'
-          }],
-        footer: 'Sample of footer. See <a href=\"https://github.com/Albejr/ngx-timeline\" target=\"_blank\">more details</a>'
-      }
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 15:52:23'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 14:26:23'),
+        header: 'UIT',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 14:12:49'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 13:57:10'),
+        header: 'UIT',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 13:45:32'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 13:20:08'),
+        header: 'UIT',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 11:05:15'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 10:15:22'),
+        header: 'UIT',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 09:30:45'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 08:13:53'),
+        header: 'UIT',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 07:34:21'),
+        header: 'IN',
+        body: [
+        ]
+      },
+      {
+        datetime: new Date('2020-05-11 06:44:34'),
+        header: 'UIT',
+        body: [
+        ]
+      },
     ];
   }
-  
-
 }
